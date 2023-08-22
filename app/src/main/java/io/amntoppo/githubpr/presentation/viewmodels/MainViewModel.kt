d9701cb7446d2e345f7effb5b2ead1f27e7c1671
amntoppo <amntoppo@gmail.com>
2023-08-22 14:17:47 +0530

    Resolve bug

--- app/src/main/java/io/amntoppo/githubpr/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/io/amntoppo/githubpr/presentation/viewmodels/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.amntoppo.githubpr.data.repository.GithubRepository
 import io.amntoppo.githubpr.domain.model.PullRequest
+import io.amntoppo.githubpr.domain.repository.IGithubRepository
 import io.amntoppo.githubpr.utils.Resource
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
@@ -11,7 +12,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    val githubRepository: GithubRepository
+    val githubRepository: IGithubRepository
 ): ViewModel() {
     private val _pullRequestStateFlow = MutableStateFlow<Resource<out List<PullRequest>>>(Resource.Loading(null))
     val pullRequestStateFlow = _pullRequestStateFlow.asStateFlow()
@@ -20,6 +21,9 @@ class MainViewModel @Inject constructor(
 
     //Demonstration of StateFlow as a State holder, substitute of LiveData
     fun getPullRequest(repo: String) {
+        if(repo.isEmpty()) {
+            _pullRequestStateFlow.value = Resource.Error(Throwable("The repository name is Empty"))
+        }
         viewModelScope.launch {
             githubRepository.getClosedPullRequest(repo).collect {
                 _pullRequestStateFlow.value = it
